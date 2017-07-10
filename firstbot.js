33583d764877cec225205261a615720286e7e777
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-07-10 16:18:19 +0100

    initial commit

---
+++ firstbot.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const privateStuff = require("./token");
+var token = privateStuff.key
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Ping/pong
+client.on('message', message => {
+  if (message.content.includes('ping')||message.content.includes('Ping')) {
+    message.reply('pong');
+  }
+});
+
+// Enabler
+client.on('message', message => {
+  console.log(message.content)
+  if (message.content.includes('sad')) {
+    const messages = ['I don\'t care', 'You deserve what\'s coming to you', 'Kill yourself'];
+    var randomIndex = Math.round(Math.random(messages.length-1)+1);
+    message.reply(messages[randomIndex]);
+  }
+});
+
+client.login(token);
