dc19838c16d255e51a4ddf997b40ccae5c8bf40f
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-07-10 18:00:56 +0100

    commit end of day 10072017

--- firstbot.js
+++ firstbot.js
@@ -7,27 +7,17 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Ping pong
 client.on('message', message => {
-  if (message.content.includes('ping')||message.content.includes('Ping')) {
-    message.reply('pong');
-  }
-});
-
-// Enabler
-client.on('message', message => {
-  console.log(message.content)
-  if (message.content.includes('sad')) {
-    const messages = ['I don\'t care', 'You deserve what\'s coming to you', 'Kill yourself'];
-    var randomIndex = Math.round(Math.random(messages.length-1)+1);
-    message.reply(messages[randomIndex]);
-  }
-});
-
-client.on('message', message => {
-  console.log(message.content)
-  if (message.content.includes('menacing') || message.content.includes('Menacing')) {
-    message.reply("This is Alexbot. I think you meant ゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴゴ");
+  if(message.content.includes('!bot')) {
+    if(message.content.includes('hw')) {
+      message.reply('Hello World!')
+    }
+    else if(message.content.includes('beRude')) {
+      message.reply('Get lost!')
+    }
+    else if(message.content.includes('kiss')) {
+      message.reply("😘")
+    }
   }
 });
 
