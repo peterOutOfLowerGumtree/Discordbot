36ca90787c7f68094433e1f247f31df4ee371ef5
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-07-14 10:37:14 +0100

    abstract function from wolfFunc.js

--- functionality/wolfFunc.js
+++ functionality/wolfFunc.js
@@ -5,13 +5,7 @@ function wolf(args, callback) {
     wolfram.query(args, function (err, result) {
         var wolfNum;
         if (args.includes("graph")) {
-            if (result[1].subpods[1]) {
-                wolfNum = result[1].subpods[1].image;
-                callback(wolfNum);
-            } else {
-                wolfNum = result[1].subpods[0].image;
-                callback(wolfNum);
-            }
+            graphFunc(result, callback);
         } else if (args.includes("integrate") ||
             args.includes("differentiate")) {
             wolfNum = result[0].subpods[0].value;
@@ -27,6 +21,17 @@ function wolf(args, callback) {
             callback("Please enter a valid argument (+,-,*,/,^,{integrate [function]},{differentiate [function]},{graph [function]})");
         }
     });
+
+
+    function graphFunc(result, callback) {
+        if (result[1].subpods[1]) {
+            var wolfNum = result[1].subpods[1].image;
+            callback(wolfNum);
+        } else {
+            wolfNum = result[1].subpods[0].image;
+            callback(wolfNum);
+        }
+    }
 }
 
 module.exports = {
